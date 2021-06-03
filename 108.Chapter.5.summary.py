b370a9a643e87acef862fceb82047602d0753e72
Wraient Daemon <81632222+wraient@users.noreply.github.com>
2021-06-03 15:07:48 +0530

    Add files via upload

---
+++ 108.Chapter.5.summary.py
@@ -0,0 +1,27 @@
+# create list with sqaure brackets
+mixed = [1, 2, 3, "four", ["five", "6"], 6.9, True, False, None]
+# print(mixed[3])
+
+# mixed.append([10, 20 ,30])
+# print(mixed)
+
+# fruits = ["Mango", "Orange"]
+# mixed.extend(fruits)
+# print(mixed)
+
+# fruits1 = ["Grapes", "Apple"]
+# mixed.insert(3, fruits1)
+# print(mixed)
+
+# popped = mixed.pop()
+# print(popped)
+# print(mixed)
+
+# del mixed[4]
+# print(mixed)
+
+# mixed.remove(True)
+# print(mixed)
+
+for i in mixed:
+    print(i)
